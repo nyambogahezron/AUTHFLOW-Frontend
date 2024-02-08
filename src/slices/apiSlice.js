c837982be3bab4899f821a5cb58e151a4455c17f
Hezron Nyamboga <hezzyn1@gmail.com>
2024-02-08 18:09:33 +0300

    add frontend

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://deploy-test-k0b3.onrender.com' });
 
 export const apiSlice = createApi({
   baseQuery,
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    proxy: {
-      '/api': {
-        target: 'https://auth-flow-api-3e9v.onrender.com/',
-        changeOrigin: true,
-      },
-    },
-  },
 })
